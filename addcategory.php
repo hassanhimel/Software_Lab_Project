73fa05a39a8ddf4e1fb30367668194492e8e5d9d
Ashikur Rahman <arahman172081@bscse.uiu.ac.bd>
2021-08-18 03:56:01 +0630

    Creating classes of Category

--- addcategory.php
+++ addcategory.php
@@ -9,3 +9,25 @@ if ($_SESSION['email']==true) {
 include('include/header.php');
 
 ?>
+<div style="width: 70% ; margin-left: 25%; margin-top: 10% ">
+	
+
+	<form action="addcategory.php" method="post" name="categoryform"onsubmit="return validateform() ">
+		<h1>Add Categories</h1>
+		<hr>
+  <div class="form-group">
+    <label for="email"> Category: </label>
+    <input type="text" name="category" class="form-control" id="email">
+  </div>
+  
+  <div class="form-group">
+  <label for="comment">Description:</label>
+  <textarea class="form-control" rows="5" name="des" id="comment"></textarea>
+</div>
+  
+  <input type="submit" name="submit" class="btn btn-primary" value="Add Category">
+</form>
+
+
+</div>
+
