0888fb543a8bc5358640aaf0a50ac08c98da38af
Sojib <ahmedsojib1682@gmail.com>
2021-08-18 00:44:57 +0600

    Update Breadcrumb Class

--- AddNews.php
+++ AddNews.php
@@ -1,9 +1,7 @@
-<!doctype html>
-<html>
-<head>
-<title>Add News</title>
-
-</head>
-<body>
-</body>
-</html>
+<div style="margin-left:17%; width: 80%; ">
+   <ul class ="breadcrumb">
+       <li class="breadcrumb-item active"><a href="home.php">Home</a></li>
+      <li class="breadcrumb-item active"><a href="news.php">News</a></li>
+      <li class="breadcrumb-item active">Add News</li>
+    </ul>
+  </div>
