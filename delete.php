202778d21cf177c13b75ad9a30b2edb8ad33fa39
zahidul-azim <mazim171209@bscse.uiu.ac.bd>
2021-08-18 01:42:09 +0600

    query

--- delete.php
+++ delete.php
@@ -2,7 +2,12 @@
 include('database/connection.php');
 $id=$_GET['del'];
  $query = mysqli_query($conn,"delete from category where id='$id'");
-
+ if (query) {
+    echo "<script> alert('category deleted') </script> "; 
+    echo "<script>window.location='http://localhost/newsportal/category.php';</script>";
+}else{
+    echo "Please Try Again";
+}
 
 
 
