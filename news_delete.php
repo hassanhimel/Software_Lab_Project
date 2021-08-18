9b1e2a1a32e8a49debd38e0ff50ea5b4e4ffaceb
Tibriz Riad <tibrizriad076@gmail.com>
2021-08-18 13:01:39 +0600

    Add php code for database connection in news_delete feature

---
+++ news_delete.php
@@ -0,0 +1,12 @@
+<?php
+
+include('database/connection.php');
+$id=$_GET['del'];
+ $query = mysqli_query($conn,"delete from news where id='$id'");
+ if (query) {
+ 	echo "<script> alert('Newsw Deleted') </script> "; 
+ 	echo "<script>window.location='http://localhost/newsportal/news.php';</script>";
+ }else{
+ 	echo "Please Try Again";
+ }
+?>
