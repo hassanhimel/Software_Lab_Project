82bcc55c12628a57f76ad3be513e5c5113360c7e
Himel <mhimel171066@bscse.uiu.ac.bd>
2021-08-18 13:40:00 +0600

    Added About, Categorie, Archives and contruct us operations

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+ error_reporting(0);
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -155,7 +160,80 @@
                    </li>
            </ul>
     
-    
+           </div><!-- /.blog-main -->
+
+<aside class="col-md-4 blog-sidebar">
+  <div class="p-3 mb-3 bg-light rounded">
+    <h4 class="font-italic">About</h4>
+    <p class="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+  </div>
+
+   <div class="p-3">
+    <h4 class="font-italic">Categories</h4>
+    <hr>
+    <ol class="list-unstyled mb-0">
+       <?php
+       include('database/connection.php');
+        $query1=mysqli_query($conn,"select * from category");
+        while($row=mysqli_fetch_array($query1)){
+
+        
+
+       ?>
+      <li><a href="category_page.php?single=<?php echo$row['category_name'];  ?>"><?php echo $row['category_name']; ?></a></li>
+      <?php } ?>
+    </ol>
+  </div>
+
+  <div class="p-3">
+    <h4 class="font-italic">Archives</h4>
+    <ol class="list-unstyled mb-0">
+      <li><a href="#">March 2020</a></li>
+      <li><a href="#">February 2020</a></li>
+      <li><a href="#">January 2020</a></li>
+      <li><a href="#">December 2020</a></li>
+      <li><a href="#">November 2020</a></li>
+      <li><a href="#">October 2020</a></li>
+      
+    </ol>
+  </div>
+
+  <div class="p-3">
+    <h4 class="font-italic">Contact Us</h4>
+    <ol class="list-unstyled">
+      <li><a href="#"></a>Dhanmondi-15,Dhaka-1209</li>
+      <li><a href="#"></a>01863-831747</li>
+      <li><a href="#"></a>aashikrahman888@gmail.com</li>
+    </ol>
+  </div>  
+</aside><!-- /.blog-sidebar -->
+
+</div><!-- /.row -->
+
+</main><!-- /.container -->
+
+<footer class="blog-footer">
+
+<p>
+<a href="#">Back to top</a>
+</p>
+</footer>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+<script src="../../assets/js/vendor/popper.min.js"></script>
+<script src="../../dist/js/bootstrap.min.js"></script>
+<script src="../../assets/js/vendor/holder.min.js"></script>
+<script>
+Holder.addTheme('thumb', {
+bg: '#55595c',
+fg: '#eceeef',
+text: 'Thumbnail'
+});
+</script>
            
   </body>
 
