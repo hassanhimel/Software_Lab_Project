742ecf3ff5e5ad5e61d96bcc212a6e8fb2134453
sojib011171067 <88420912+sojib011171067@users.noreply.github.com>
2021-09-29 14:00:58 +0600

    Categories deleted option added to the website

--- delete.php
+++ delete.php
@@ -1,14 +1,13 @@
 <?php
+
 include('database/connection.php');
 $id=$_GET['del'];
  $query = mysqli_query($conn,"delete from category where id='$id'");
  if (query) {
-    echo "<script> alert('category deleted') </script> "; 
-    echo "<script>window.location='http://localhost/newsportal/category.php';</script>";
-}else{
-    echo "Please Try Again";
-}
-
-
-
+ 	echo "<script> alert('category deleted') </script> "; 
+ 	echo "<script>window.location='http://localhost/newsportal/category.php';</script>";
+ }else{
+ 	echo "Please Try Again";
+ }
 ?>
+
